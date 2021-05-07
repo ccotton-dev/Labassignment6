fb6d77519a32b8da671cd5d388f1f81492f6b075
unknown <ccotton@mail.fresnostate.edu>
2021-05-07 01:11:27 -0700

    finished code

--- LA6.py
+++ LA6.py
@@ -32,3 +32,30 @@ loop three times
     print output (fruit) to the user
 
 """
+
+
+import random
+
+def main():
+    for i in range(0, 3):
+        spin()
+
+def spin():
+    rand_num = random.randint(1,20)
+    output = ""
+    if (rand_num > 15):
+        output = "Cherries"
+    if (rand_num > 10):
+        output = "Orange"
+    if (rand_num > 5):
+        output = "Plum"
+    if (rand_num > 2):
+        output = "Bell"
+    if (rand_num > 1):
+        output = "Melon"
+    else:
+        output = "Bar"
+    
+    print(output, end=" ")
+
+main()
